2fecda3740dcfb12e16dab99b183e9ea02768009
Vel MAr <velmar.annabell@gmail.com>
2023-05-18 18:41:39 +0300

    add styles

--- src/components/cryptoCard/CryptoCard.js
+++ src/components/cryptoCard/CryptoCard.js
@@ -1,17 +1,23 @@
+import styles from "./CryptoCard.module.css";
 import useGetCryptoLogo from "../hooks/useGetCryptoLogo";
 
-const CryptoCard=(props)=>{
+const CryptoCard = (props) => {
     console.log(props.crypto);
     const { logo } = useGetCryptoLogo(props.crypto)
-return(
-    <li>
-        <img src={logo} alt="img" />
-        {props.crypto}
-        {props.price}
-        {props.usdSymbol}
-
-    </li>
-)
+    return (
+        <li className={styles.crypto_card}>
+            <div className={styles.crypto_card__img}>
+                <img src={logo} alt="img" />
+            </div>
+            <h2>{props.crypto}</h2>
+            <div className={styles.crypto_card__description} >
+                <div className={styles.crypto_card__price}>
+                    <span>{props.price} </span>
+                    <span>{props.usdSymbol}</span>
+                </div>
+            </div>
+        </li>
+    );
 };
 
-export default CryptoCard
+export default CryptoCard;
