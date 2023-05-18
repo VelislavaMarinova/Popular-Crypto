46058c145142a4e736b95d6ab54ec65c4b5dc9aa
Vel MAr <velmar.annabell@gmail.com>
2023-05-18 18:41:24 +0300

    add styles

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -2,6 +2,7 @@ import Loading from "../loading/Loading.js";
 import useGetData from "../hooks/useGetData.js";
 import useGetCryptoLogo from "../hooks/useGetCryptoLogo.js";
 import CryptoCard from "../cryptoCard/CryptoCard.js";
+import styles from './HomePage.module.css'
 
 const HomePage = () => {
     const { data, loading } = useGetData();
@@ -12,23 +13,27 @@ const HomePage = () => {
     if (loading || loadingLogo) {
         return <Loading />;
     };
+    if (!data) {
+        return (
+            <h2 className='expenses-list__fallback'>
+                Found no data.
+            </h2>
+        );
+    }
 
     return (
-        <section >
+        <section className={styles.main_section}>
             <h1>Top Volume Cryptocurrencies</h1>
-            {data ?
-                <ul >
-                    {data.map(x =>
-                        <CryptoCard
-                            key={Math.random()}
-                            crypto={x.CoinInfo.Name}
-                            price={x.RAW.USD.PRICE}
-                            usdSymbol={x.RAW.USD.TOSYMBOL}>
-                        </CryptoCard>
-                    )}
-                </ul> :
-                <p >No data available!</p>
-            }
+            <ul >
+                {data.map(x =>
+                    <CryptoCard
+                        key={Math.random()}
+                        crypto={x.CoinInfo.Name}
+                        price={x.RAW.USD.PRICE}
+                        usdSymbol={x.RAW.USD.TOSYMBOL}>
+                    </CryptoCard>
+                )}
+            </ul>
         </section>
     );
 };
